c3fa3315c8d514336bca7814045e737425efb4f3
chandrika1993 <you@example.com>
2018-06-04 18:20:41 +0530

    doc-comment added

--- src/main/java/com/friendmanagement/dto/SuccessStatusDto.java
+++ src/main/java/com/friendmanagement/dto/SuccessStatusDto.java
@@ -1,5 +1,16 @@
 package com.friendmanagement.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+
+/**
+ * <PRE>
+ * Class name       : SuccessStatusDto
+ * Description      : DTO for holding Success Status Information. 
+ * Author           : Capgemini.
+ * </PRE>
+ */
+@JsonAutoDetect(fieldVisibility = Visibility.ANY)
 public class SuccessStatusDto {
 
     private boolean success;
