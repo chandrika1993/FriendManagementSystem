cb5394cb26df050203ec72df1f6526e240004329
chandrika1993 <you@example.com>
2018-06-05 12:15:41 +0530

    Swagger-added

---
+++ src/main/java/com/friendmanagement/swagger/SwaggerConfig.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.friendmanagement.swagger;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger API Configuration
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    /**
+     * @return Docket
+     */
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.friendmanagement"))
+                .paths(regex("/friendmanagementservice.*")).build().apiInfo(metaData());
+    }
+
+    /**
+     * @return APIInfo
+     */
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo("Friend Management System", "REST Api",
+                "1.0", "Terms & Conditions", new Contact("Chandrika Mohan", "",
+                        "chandrika.a.mohan@capgemini.com"),
+                "", "");
+        return apiInfo;
+    }
+}
